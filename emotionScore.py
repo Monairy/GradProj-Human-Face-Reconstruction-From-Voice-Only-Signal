81b1ccc57950a2e57ced7d4a065e2cf08ba034d4
AhmadAbdElHakim <ahmad-abdelhakim@hotmail.com>
2021-03-25 08:28:01 +0200

    Update emotionScore.py

--- emotionScore.py
+++ emotionScore.py
@@ -1,10 +1,4 @@
 # pylint: disable=no-member
-import os
-leftImages = []
-det = []
-tangle = []
-pangle = []
-rangle = []
 
 def detect_faces(path):
     """Detects faces in an image."""
@@ -41,6 +35,7 @@ def detect_faces(path):
         p = face.pan_angle
         r = face.roll_angle
 
+        # Exporting values to text file
         print('Detection confidence:{}'.format(d))
         print('anger: {}'.format(a))
         print('joy: {}'.format(j))
@@ -53,27 +48,29 @@ def detect_faces(path):
         print('pan angle: {}'.format(p))
         print('roll angle: {}'.format(r))
 
-        # Appending values to lists
-        det.append(d)
-        tangle.append(t)
-        pangle.append(p)
-        rangle.append(r)
+        # Exporting values to excel sheet
+        outSheet.write(i + 1, 2, d)
+        outSheet.write(i + 1, 3, t)
+        outSheet.write(i + 1, 4, p)
+        outSheet.write(i + 1, 5, r)
 
         # Checking if the image meets the critira
         if d > 0.5 and a == 'VERY_UNLIKELY' and j == 'VERY_UNLIKELY' and su == 'VERY_UNLIKELY' and so == 'VERY_UNLIKELY' and h == 'VERY_UNLIKELY' and b == 'VERY_UNLIKELY' and u == 'VERY_UNLIKELY' and  t < 5 and t > -5 and p < 5 and p > -5 and r < 5 and r > -5:
             leftImages.append('1')
         else:
             leftImages.append('0')
-
+        
         # Face bounds are not needed, but just in case ¯\_(ツ)_/¯
         vertices = (['({},{})'.format(vertex.x, vertex.y)
                     for vertex in face.bounding_poly.vertices])
-        print('face bounds: {}'.format(','.join(vertices)))
+        print('face bounds: {} \n'.format(','.join(vertices)))
 
     # A flag to mark finding at least one face; because some images are so distorted that no face can be detected
     if flag == 0:
         # Appending zero for distorted image; in order not to mess the list ordering
         leftImages.append('0')
+    
+    outSheet.write(i + 1, 1, leftImages[i])
     
     # Error Handling
     if response.error.message:
@@ -82,34 +79,10 @@ def detect_faces(path):
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
 
-import sys
-# Change to the path of the directory containing the images
-path = '/home/ahmad/Downloads/GraduationProject/API/Images/'
-images = os.listdir(path)
-# Redirecting stdout to output to the text file
-sys.stdout = open('Output.txt', 'w')
-names = []
-fullPath = []
-# Converting Images names to integer to sort
-for image in images:
-    names.append(int(image.replace('.jpg','')))
-# For some reason it has to be sorted
-names.sort()
-# For each image, calling the API and indexing each image with its number
-for n in names:
-    print(n)
-    image = path + str(n) + '.jpg'
-    fullPath.append(image)
-    detect_faces(image)
-    print('\n')
-# Printing the boolean values as backup for excel
-print(leftImages)
-sys.stdout.close()
+import os, sys, xlsxwriter
 
-import xlsxwriter
 outWorkbook = xlsxwriter.Workbook('Output.xlsx')
 outSheet = outWorkbook.add_worksheet()
-
 # Exporting the path, boolean value, Detection confidence, and angles of each image, to be used for deletion
 outSheet.write('A1','Path')
 outSheet.write('B1','Bool')
@@ -118,12 +91,36 @@ outSheet.write('D1','Tilt')
 outSheet.write('E1','Pan')
 outSheet.write('F1','Roll')
 
-for item in range(len(fullPath)):
-    outSheet.write(item + 1, 0, fullPath[item])
-    outSheet.write(item + 1, 1, leftImages[item])
-    outSheet.write(item + 1, 2, det[item])
-    outSheet.write(item + 1, 3, tangle[item])
-    outSheet.write(item + 1, 4, pangle[item])
-    outSheet.write(item + 1, 5, rangle[item])
-
+# Change to the path of the directory containing the images
+path = '/home/ahmad/Downloads/GraduationProject/API/Images/'
+# Getting the names of files in the directory
+images = os.listdir(path)
+# Redirecting stdout to output to the text file
+sys.stdout = open('Output.txt', 'w')
+# Iterator to keep track of images
+i = 0
+leftImages = []
+names = []
+# Converting Images names to integer to sort
+for image in images:
+    names.append(int(image.replace('.jpg','')))
+# For some reason it has to be sorted; because it was random
+names.sort()
+# For each image, calling the API and indexing each image with its number
+for n in names:
+    print(n)
+    image = path + str(n) + '.jpg'
+    outSheet.write(i + 1, 0, image)
+    # An exception handling block; because it keeps getting timeout error
+    try:
+        detect_faces(image)
+    except Exception as m:
+        sys.__stdout__.write('Last image is: {}'.format(n) + '\n' + m + '\n')
+        print(leftImages)
+        sys.stdout.close()
+        outWorkbook.close()
+    else:
+        i += 1
+print(leftImages)
+sys.stdout.close()
 outWorkbook.close()
